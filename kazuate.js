67c599bc88a34fb542269d0c0b541911c66134de
G278412022 <227841@st.takushoku-u.ac.jp>
2022-05-12 14:20:04 +0900

    ヘンコウ

---
+++ kazuate.js
@@ -0,0 +1,18 @@
+//答え
+let kotae = Math.floor(Math.randamu()*10+1);
+console.log('答え'+kotae);
+
+//入力回数（予想回数）
+let kaisu =0;
+
+//予想を4回実行する
+//将来：ボタンを押したら、hantei()を呼び出すように修正する
+hantei();
+hantei();
+hantei();
+hantei();
+
+//ボタンを押したら後の処理をする関数　hantei()の定義
+function hantei(){
+    let yoso=4;
+}

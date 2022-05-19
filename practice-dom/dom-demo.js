a3970b7118eb642a5ad6f13f2fcb50e66f2dfe8a
G278412022 <227841@st.takushoku-u.ac.jp>
2022-05-19 14:32:19 +0900

    変更

--- practice-dom/dom-demo.js
+++ practice-dom/dom-demo.js
@@ -1,6 +1,6 @@
 // id が sevenOcean の h2 要素を検索
 // （次の行を完成させる）
-let h2sevenOcean = document.querySelector('h2#sevenOcean');              ;
+let h2sevenOcean = document.querySelector('h2#sevenOcean');
 
 // 7大洋　の名前を配列に代入
 let oceans = [

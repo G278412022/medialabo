0e0fe4fd38bfd5776862d9ae960b9195bd1263c3
G278412022 <227841@st.takushoku-u.ac.jp>
2022-05-19 14:17:14 +0900

    変更

---
+++ practice-dom/dom-dom.js
@@ -0,0 +1,28 @@
+// id が sevenOcean の h2 要素を検索
+// （次の行を完成させる）
+let h2sevenOcean = document.querySelector('h2#sevenOcean');              ;
+
+// 7大洋　の名前を配列に代入
+let oceans = [
+  '北極海',
+  '北大西洋',
+  '南大西洋',
+  'インド洋',
+  '北太平洋',
+  '南太平洋',
+  '南極海'
+]
+
+// ul 要素を新規作成する．（次の行を完成させる）
+ul = document.createElement('ul');
+
+// ul 要素を h2sevenOcean の次に追加する（次の行を書く）
+h2sevenOcean.insertAdjacentElement('afterend',ul);
+
+// ul に7つの大陸を追加する
+for (let o of oceans) {
+  // （この中を完成させる）
+let li = document.createElement('li');
+li.textContent = 0;
+ul.insertAdjacentElement('beforend',li);
+}

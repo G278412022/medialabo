f12006a47f8e1fff0b3fa847cc9dc1b61341e06a
G278412022 <227841@st.takushoku-u.ac.jp>
2022-05-23 18:24:01 +0900

    変更

--- kazuate.js
+++ kazuate.js
@@ -7,28 +7,37 @@ let kaisu =0;
 //予想を4回実行する
 //将来：ボタンを押したら、hantei()を呼び出すように修正する
 hantei();
-hantei();
-hantei();
-hantei();
 
 //ボタンを押したら後の処理をする関数　hantei()の定義
 function hantei(){
     let yoso=4;
       kaisu=kaisu+1;
+      let s = document.querySelector('span#kaisu');
+      s.textContent = kaisu;
+      let t = document.quarySelector('span#yoso');
+      t.textContent = yoso;
          (`${kaisu}回目の予想${yoso}`);
         if(kaisu>=4){
             console.log("答えは"+kotae+"でした。すでにゲームは終わっています。");
+            console.log= i;
         }if(kotae==yoso&&kaisu==1){
             console.log("正解です。おめでとう！");
+            console.log =i;
         }
         if(kaisu==3&&kotae!=yoso){
             console.log("間違い残念でした答えは"+kotae+"でした。");
+            console.log=i;
         }
         if(kotae>yoso&&kaisu<3){
             console.log("間違い。答えはもっと大きいですよ。");
+            console.log=i;
         }
         if(kotae<yoso&&kaisu<3){
-            console.log("間違い。答えはもっと小さいですよ。")
+            console.log("間違い。答えはもっと小さいですよ。");
+            console.log=i;
         }
+
+        let i =document.querySelector('p#result');
+        i.textContent = resulet;
 
 }
